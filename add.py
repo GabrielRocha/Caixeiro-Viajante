e32311feb98d374bdeac75937f6a2c9e8ea85d15
Gabriel Rocha <gabriel.rocha.gbr@gmail.com>
2013-06-14 10:17:58 -0300

    Update

--- add.py
+++ add.py
@@ -1,3 +1,5 @@
+#!/bin/python
+# encoding: utf-8
 import networkx as nx
 import matplotlib.pyplot as plt
 class Add():
@@ -6,23 +8,24 @@ class Add():
         self.path = {}
         self.path_vertex = []
 
-    def select_vertex_initial(self, vertexs):
-        for origem in self.graph.keys():
-            for vertex, weight in self.graph.get(origem).iteritems():
-                if vertex in vertexs:
-                    self.path[(origem,vertex)] = weight
-        print "Caminho inicial: %s \n" %self.path
+    def set_vertex_initial(self, vertexs):
+        for position in self.graph.keys():
+            for vertex, weight in self.graph.get(position).iteritems():
+                if vertex in vertexs and position in vertexs and not((vertex,position)) in self.path:
+                    self.path[(position,vertex)] = weight
+        print "First Path: %s \n" %self.path
 
     def get_path_vertexs(self):
         for position in range(len(self.path.keys())):
             for vertex in self.path.keys()[position]:
                 if not(vertex) in self.path_vertex:
                     self.path_vertex.append(vertex)
+        self.path_vertex.sort()
                     
     def calculate_way(self):
+        self.get_path_vertexs()        
         while self.path_vertex != self.graph.keys():
             menor_custo = 999999
-            self.get_path_vertexs()
             for vertex_graph in self.graph.keys():
                 if not(vertex_graph in self.path_vertex):
                     for link_vertex in self.path.keys():
@@ -32,23 +35,23 @@ class Add():
                         custo -= self.path.get(link_vertex)
                         if custo < menor_custo:
                             menor_custo = custo
-                            chave = link_vertex
+                            link_vertex_menor_custo = link_vertex
                             vertex_menor_custo = vertex_graph
-            print "Menor Custo: %s\nLigacao dos Vertices: %s" %(menor_custo, chave)
-            self.mudar_path(chave, vertex_menor_custo)
+            print "Menor Custo: %s\nLigacao dos Vertices: %s %s" %(menor_custo, vertex_menor_custo, link_vertex_menor_custo)
+            self.change_path(link_vertex_menor_custo, vertex_menor_custo)
     
-    def mudar_path(self, chave,origem):
-        self.path.pop(chave)
-        self.path[(origem,chave[0])] = self.graph.get(chave[0]).get(origem)
-        self.path[(origem,chave[1])] = self.graph.get(chave[1]).get(origem)
+    def change_path(self, link_vertex, vertex):
+        self.path.pop(link_vertex)
+        self.path[(vertex,link_vertex[0])] = self.graph.get(link_vertex[0]).get(vertex)
+        self.path[(vertex,link_vertex[1])] = self.graph.get(link_vertex[1]).get(vertex)
         self.get_path_vertexs()
         print "Caminho: %s \n" %self.path
     
-    def get_custo_total(self):
-        soma = 0
-        for peso in self.path.values():
-            soma += peso
-        return soma
+    def get_total_cost(self):
+        total = 0
+        for weight in self.path.values():
+            total += weight
+        return total
 
     def show_graph(self):
         graph=nx.Graph(self.graph)
@@ -66,7 +69,7 @@ class Add():
         plt.text(0.5, 0.97, "Path: "+str(self.path),
                             horizontalalignment='center',
                             transform=plt.gca().transAxes)
-        plt.text(0.5, 0.90, "Custo Total: "+str(self.get_custo_total()),
+        plt.text(0.5, 0.90, "Custo Total: "+str(self.get_total_cost()),
                            horizontalalignment='center',
                            transform=plt.gca().transAxes)
         plt.axis('off')
@@ -77,7 +80,7 @@ class Add():
         for position in self.graph.keys():
             labels[position]=position
         return labels
-
+    
     def get_list_weights_edge(self):
         list_weights_edge={}
         for position in self.graph.keys():
@@ -88,14 +91,21 @@ class Add():
 
 if __name__ == '__main__':
    print "Exemplo 1 - Graph"
-   graph = { 
-           1: { 2: 3, 3: 4, 4: 3, 5: 6  },
-           2: { 3: 1, 4: 7, 5: 4  },
-           3: { 4: 2, 5: 5  },
-           4: { 5: 5  },
-           5: {  },
-           }
+   # graph = { 
+   #            1: { 2: 3, 3: 4, 4: 3, 5: 6  },
+   #            2: { 3: 1, 4: 7, 5: 4  },
+   #            3: { 4: 2, 5: 5  },
+   #            4: { 5: 5  },
+   #            5: {  },
+   #            }
+   graph = {
+        1: { 2: 3, 3: 4, 4: 3, 5: 6 },
+        2: { 1: 3, 3: 1, 4: 7, 5: 4 },
+        3: { 1: 4, 2: 1, 4: 2, 5: 5 },
+        4: { 1: 3, 2: 7, 3: 2, 5: 5 },
+        5: { 1: 6, 2: 4, 3: 5, 4: 5 },
+   }
    add = Add(graph)
-   add.select_vertex_initial([1,2,3])
+   add.set_vertex_initial([5,4,3])
    add.calculate_way()
    add.show_graph()
